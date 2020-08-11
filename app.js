4a0d87e53435d15f45fb52a9326d87d2c07fcda0
Claire LI <lijiaoni019@163.com>
2020-08-11 20:43:23 +0800

    project init

---
+++ app.js
@@ -0,0 +1,12 @@
+App({
+  onLaunch: function() {
+    wx.BaaS = requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    wx.BaaS.wxExtend(wx.login,
+     wx.getUserInfo,
+     wx.requestPayment)
+
+    wx.BaaS.init('919ec66516189a58d8e1')
+    wx.BaaS.auth.loginWithWechat() // 静默登录
+  },
+})
