9d5684beed3aa5fc3ada7b642d51409b048c5d6a
Claire LI <lijiaoni019@163.com>
2020-08-13 19:39:13 +0800

    first Version

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,104 @@
 Page({
+  data:{
+    nbFrontColor: '#000000',
+    nbBackgroundColor: '#ffffff',
+    p:1,
+  },
+
+  checkCurrentUser: function () {
+    wx.BaaS.auth.loginWithWechat().then(currentUser => {
+    this.setData({currentUser})
+    this.getDataFromBaasFavorite()
+  })
+},
+
+
+  getDataFromBaasBar: function () {
+    let Bar = new wx.BaaS.TableObject("bar")
+    let query = new wx.BaaS.Query()
+    Bar.setQuery(query).orderBy(['-created_at']).limit(6).find().then (res => {
+      console.log(res)
+      let bar = res.data.objects
+      this.setData({bar})
+      console.log(this.data)
+    })
+
+  },
+
+  // getDataFromBaasFavorite: function () {
+  //   let id = this.data.currentUser.id
+  //   console.log(id)
+  //   let Favorite = new wx.BaaS.TableObject("favorite")
+  //   let query = new wx.BaaS.Query()
+  //   query.compare('user_id', '=', id)
+  //   Favorite.setQuery(query).orderBy(['-created_at']).find().then (res => {
+  //     console.log(res)
+  //     let favorite = res.data.objects
+  //     this.setData({favorite})
+  //     console.log(this.data)
+  //   })
+
+  // },
+
+  onReachBottom:function(){
+    wx.showLoading({
+      title: 'Loading',
+     })
+
+     setTimeout(()=>{
+      let p = this.data.p
+      p = p + 1;
+      console.log(p)
+      this.setData({p})
+      console.log(this.data.p)
+
+      if(p<=8){
+      let Bar = new wx.BaaS.TableObject("bar")
+      let query = new wx.BaaS.Query()
+      Bar.setQuery(query).orderBy(['-created_at']).limit(6*p).offset(0).find().then (res => {
+        wx.hideLoading()
+        console.log(res)
+        let bar = res.data.objects
+        this.setData({bar})
+        console.log(this.data)
+      })}else{
+        wx.showToast({
+          title: 'No more content',
+          duration: 2000,
+          icon:"none"
+        })
+      }
+     },1000)
+   
+    },
+
+     //发起请求
+
+  navigateToShowPage:function(e){
+    console.log(e)
+    let id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: `../show/show?id=${id}`,
+    })
+
+  },
+
+
+  navigateToUser: function () {
+    wx.navigateTo({
+      url: "../user/user"
+    })
+  },
+
+  
+  onLoad: function () {
+    this.getDataFromBaasBar()
+    this.checkCurrentUser()
+    // let list = wx.getStorageSync('list')
+    // if (list){this.setData({list})}else{
+    //   let list = [];
+    //   this.setData({list})
+    // }
+  },
   
 })
---
+++ pages/user/user.js
@@ -0,0 +1,105 @@
+// pages/user/user.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  navigateToHome: function () {
+    wx.navigateTo({
+      url: "../index/index"
+    })
+  },
+
+  checkCurrentUser: function () {
+    let currentUser = wx.getStorageSync('currentUser')
+    if (currentUser){
+      this.setData({currentUser})
+      this.setData({user:true})
+    }else {
+      this.setData({user:true})
+    }
+
+
+  },
+
+  userLogin: function (data) {
+    wx.BaaS.auth.loginWithWechat(data).then(currentUser => {
+    currentUser = currentUser.toJSON()
+    this.setData({currentUser})
+    wx.setStorageSync('currentUser',currentUser)
+    }, err => {
+     wx.showModal({
+       cancelColor: 'cancelColor',
+       title: 'Authorization Needed for Access',
+       
+     })
+ })
+
+},
+
+userLogout: function () {
+  wx.BaaS.auth.logout().then(res => {
+    wx.setStorageSync('currentUser', null);
+    this.setData({currentUser: null});
+  });
+}, 
+
+
+  onLoad: function (options) {
+    this.checkCurrentUser()
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
